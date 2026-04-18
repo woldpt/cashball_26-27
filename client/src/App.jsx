bccb9e5f609a4569b1b58dc7f0fa590a866ef156
woldpt <woldpt@set84.com>
2026-04-18 08:20:54 +0100

    field size

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7873,7 +7873,7 @@ function App() {
                                   <div
                                     className="relative w-full"
                                     style={{
-                                      aspectRatio: "9/16",
+                                      aspectRatio: "9/14",
                                       background:
                                         "linear-gradient(180deg, #05430e 0%, #0b5e1a 50%, #05430e 100%)",
                                     }}
